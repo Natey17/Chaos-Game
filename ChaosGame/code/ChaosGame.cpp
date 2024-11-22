34a7a0533f2e9c6c16e11c479212c202d52ef609
Nate York <50311051+Natey17@users.noreply.github.com>
2024-11-21 16:42:14 -0800

    Update ChaosGame.cpp

--- ChaosGame/code/ChaosGame.cpp
+++ ChaosGame/code/ChaosGame.cpp
@@ -15,9 +15,21 @@ int main()
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game!!", Style::Default);
-	
-	vector<Vector2f> vertices;
-	vector<Vector2f> points;
+
+	Text infoText;
+	Font textFont;
+	textFont.loadFromFile("fonts/arial.ttf");
+	infoText.setFont(textFont);
+	infoText.setString("Pick Your Points");
+	infoText.setCharacterSize(75);
+	infoText.setFillColor(Color::White);
+	FloatRect textBounds = infoText.getLocalBounds();
+	infoText.setOrigin(textBounds.left + textBounds.width / 2.0f, textBounds.top + textBounds.height / 2.0f);
+	infoText.setPosition(1920 / 2.0f, 75);
+
+	srand(time(NULL));
+	vector<Vector2f> baseVertices;
+	vector<Vector2f> chaosPoints;
 
 	while (window.isOpen())
 	{
@@ -26,33 +38,40 @@ int main()
 		Handle the players input
 		****************************************
 		*/
-		Event event;
-		while (window.pollEvent(event))
+		Event userInput;
+		while (window.pollEvent(userInput))
 		{
-		    if (event.type == Event::Closed)
-		    {
-					// Quit the game when the window is closed
-					window.close();
-		    }
-		    if (event.type == sf::Event::MouseButtonPressed)
-		    {
-			if (event.mouseButton.button == sf::Mouse::Left)
+			if (userInput.type == Event::Closed)
 			{
-			    std::cout << "the left button was pressed" << std::endl;
-			    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
-			    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-	
-			    if(vertices.size() < 3)
-			    {
-				vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
-			    }
-			    else if(points.size() == 0)
-			    {
-				///fourth click
-				///push back to points vector
-			    }
+				// Quit the game when the window is closed
+				window.close();
 			}
-		    }
+			if (userInput.type == sf::Event::MouseButtonPressed)
+			{
+				if (userInput.mouseButton.button == sf::Mouse::Left)
+				{
+
+					std::cout << "the left button was pressed" << std::endl;
+					std::cout << "mouse x: " << userInput.mouseButton.x << std::endl;
+					std::cout << "mouse y: " << userInput.mouseButton.y << std::endl;
+
+					if (baseVertices.size() < 3)
+					{
+						baseVertices.push_back(Vector2f(userInput.mouseButton.x, userInput.mouseButton.y));
+						if (baseVertices.size() == 3)
+						{
+							infoText.setString("Choose Start Point");
+							FloatRect textBounds = infoText.getLocalBounds();
+							infoText.setOrigin(textBounds.left + textBounds.width / 2.0f, textBounds.top + textBounds.height / 2.0f);
+							infoText.setPosition(1920 / 2.0f, 75);
+						}
+					}
+					else if (chaosPoints.size() == 0)
+					{
+						chaosPoints.push_back(Vector2f(userInput.mouseButton.x, userInput.mouseButton.y));
+					}
+				}
+			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
@@ -63,27 +82,44 @@ int main()
 		Update
 		****************************************
 		*/
-	
-		if(points.size() > 0)
+
+		if (chaosPoints.size() > 0)
 		{
-		    ///generate more point(s)
-		    ///select random vertex
-		    ///calculate midpoint between random vertex and the last point in the vector
-		    ///push back the newly generated coord.
+			for (int i = 0; i < 10; i++)
+			{
+				int randomVertex = rand() % 3;
+				float midX = (baseVertices[randomVertex].x + chaosPoints.back().x) * 0.5;
+				float midY = (baseVertices[randomVertex].y + chaosPoints.back().y) * 0.5;
+				chaosPoints.push_back(Vector2f(midX, midY));
+			}
 		}
-	
+
 		/*
 		****************************************
 		Draw
 		****************************************
 		*/
 		window.clear();
-		for(int i = 0; i < vertices.size(); i++)
+
+		if (baseVertices.size() < 3 || chaosPoints.size() == 0)
 		{
-		    RectangleShape rect(Vector2f(10,10));
-		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-		    rect.setFillColor(Color::Blue);
-		    window.draw(rect);
+			window.draw(infoText);
+		}
+
+		for (int i = 0; i < baseVertices.size(); i++)
+		{
+			RectangleShape baseMarker(Vector2f(10, 10));
+			baseMarker.setPosition(Vector2f(baseVertices[i].x, baseVertices[i].y));
+			baseMarker.setFillColor(Color::Blue);
+			window.draw(baseMarker);
+		}
+
+		for (int i = 0; i < chaosPoints.size(); i++)
+		{
+			RectangleShape chaosMarker(Vector2f(5, 5));
+			chaosMarker.setPosition(Vector2f(chaosPoints[i].x, chaosPoints[i].y));
+			chaosMarker.setFillColor(Color::Red);
+			window.draw(chaosMarker);
 		}
 		window.display();
 	}
